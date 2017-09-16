69302b47907b9d7c9f3d0fc33ff848d6785682dd
t_yamazoe <t_yamazoe@dynabook_R63PS>
2017-09-16 17:11:06 +0900

    commit

---
+++ src/jp/a_frontier/iot/server/Test001.java
@@ -0,0 +1,12 @@
+package jp.a_frontier.iot.server;
+
+public class Test001 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Test001");
+
+	}
+
+}
